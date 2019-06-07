dca33543070205219d0ecfc824f81d034ff292f7
cbenisson <blessebbr183@gmail.com>
2019-06-07 15:09:31 +0000

    resetted config

--- server/config.py
+++ server/config.py
@@ -11,9 +11,9 @@ class Config(object):
     SQLALCHEMY_TRACK_MODIFICATIONS = True
     PROJECTS_PER_PAGE = 10
     WHOOSH_BASE = os.path.join(basedir, 'whoosh_base')
-    MAIL_SERVER = 'smtp.gmail.com'
-    MAIL_PORT = 587
-    MAIL_USE_TLS = True
-    MAIL_USERNAME = 'knights.of.coe3@gmail.com'
-    MAIL_PASSWORD = 'coe3solutionarchitects'
+    MAIL_SERVER = os.environ.get('MAIL_SERVER') or 'smtp.gmail.com'
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 587)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') or True
+    MAIL_USERNAME = os.environ.get('MAIL_USER')
+    MAIL_PASSWORD = os.environ.get('MAIL_PSWD')
     ADMINS = ['knights.of.coe3@gmail.com']
